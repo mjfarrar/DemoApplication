d72d8fcaeae17a69b4dfb0868d2af1d1808f8afe
mjfarrar <michaeljohnfarrar@gmail.com>
2019-03-05 08:49:27 -0500

    adding controller and view to pull data through helper from API

---
+++ DemoApplication/DemoApplication/Areas/Sports/Controllers/SportsController.cs
@@ -0,0 +1,22 @@
+﻿using DemoApplication.Areas.Sports.Helpers;
+using DemoApplication.Areas.Sports.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace DemoApplication.Areas.Sports.Controllers
+{
+    public class SportsController : Controller
+    {
+        private SportsHelper sportHelper = new SportsHelper();
+
+        public ActionResult SportsList() 
+        {
+            SportListModel model = sportHelper.GetSportListModel();
+            return View(model);
+        }
+    }
+}
---
+++ DemoApplication/DemoApplication/Areas/Sports/Helpers/SportsHelper.cs
@@ -0,0 +1,55 @@
+﻿using DemoApplication.Areas.Sports.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using RestSharp;
+
+namespace DemoApplication.Areas.Sports.Helpers
+{
+    public class SportsHelper
+    {
+        string sportsAPI = ConfigurationManager.AppSettings["SportsAPIURL"] + "api/sports";
+
+        //public IEnumerable<SportModel> GetAllSports() 
+        //{
+        //    IEnumerable<SportModel> sports = null;
+
+        //    using (var client = new HttpClient())
+        //    {
+        //        HttpResponseMessage response = client.GetAsync(sportsAPI + "GetAllSports").Result;
+
+        //        if (response.IsSuccessStatusCode)
+        //        {
+        //            string data = response.Content.ReadAsStringAsync().Result;
+        //            JavaScriptSerializer jsSerializer = new JavaScriptSerializer();
+        //            sports = jsSerializer.Deserialize<List<SportModel>>(data);
+        //        }
+        //    }
+
+        //    return sports;
+        //}
+
+        public IEnumerable<SportModel> GetAllSports()
+        {
+            var client = new RestClient(sportsAPI);
+            var request = new RestRequest("GetAllSports", Method.GET);
+
+            var response = client.Execute(request);
+
+            IEnumerable<SportModel> sports = Newtonsoft.Json.JsonConvert.DeserializeObject<IEnumerable<SportModel>>(response.Content);
+            return sports;
+        }
+
+        public SportListModel GetSportListModel() 
+        {
+            SportListModel model = new SportListModel();
+            model.SportsList = GetAllSports();
+            return model;
+        }
+    }
+}
