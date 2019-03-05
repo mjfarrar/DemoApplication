82bfd9ef25f7494eb7cb02eb78330921d868323e
mjfarrar <michaeljohnfarrar@gmail.com>
2019-03-04 23:44:51 -0500

    Adding untracked files

---
+++ DemoApplication/DemoApplication/CustomViewEngine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoApplication
+{
+    public class CustomViewEngine : RazorViewEngine
+    {
+        public CustomViewEngine(){
+            ViewLocationFormats = new[]
+            {
+                "~/Areas/{1}/Views/{0}.cshtml",
+                "~/Areas/Shared/Views/{0}.cshtml"
+            };
+            PartialViewLocationFormats = ViewLocationFormats;
+            MasterLocationFormats = ViewLocationFormats;
+        }
+    }
+}
